5ff615f037a17f7234d806ea6c38daa18470ea26
Nouster <djebali.mohamed88@gmail.com>
2023-04-11 11:54:05 +0200

    new method to get one item from resource

--- public/index.php
+++ public/index.php
@@ -10,12 +10,24 @@ $dotenv = new Dotenv();
 $dotenv->loadEnv('.env');
 $pdo = DbConfig::getPdoInstance();
 
-header('Content-type: application/json; charset=UTF-8');
-
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
+$uriParts = explode("/", $uri);
+$uriPartsCount = count($uriParts);
+var_dump($uriPartsCount);
+$resource = $uriParts[1];
+$id = intval($uriParts[2]);
+
+
+header('Content-type: application/json; charset=UTF-8');
+
+
 $crud = new QuizzCrud($pdo);
 
 if($uri === "/quizz" && $method === "GET"){
    echo json_encode($crud->getAllQuestions()); 
+}
+
+if ($uriPartsCount === 3 && $uriParts[1] === "quizz" && $method === "GET") {
+    echo json_encode($crud->getOneQuestion($id));
 }
