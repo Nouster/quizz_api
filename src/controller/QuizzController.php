2cb04c5967c7ea0fd7ebbedd30e827afa9e92775
Nouster <djebali.mohamed88@gmail.com>
2023-04-12 15:57:03 +0200

    Refacto in controller method to get a question

--- src/controller/QuizzController.php
+++ src/controller/QuizzController.php
@@ -30,53 +30,9 @@ class QuizzController
 
         $this->checkCollectionVerbs();
         $this->checkResourceVerbs();
+        $this->handleCollectionGet();
+        $this->handleResourceGet();
 
-        if ($uri === "/quizz" && $method === "GET") {
-            try {
-                http_response_code(200);
-                echo json_encode($this->crud->getAllQuestions());
-            } catch (InvalidArgumentException $e) {
-                http_response_code(400);
-                echo json_encode([
-                    "error" => "An error occured",
-                    "code" => 400,
-                    "message" => "The specified ID is not valid."
-                ]);
-            } catch (OutOfBoundsException $e) {
-                http_response_code(404);
-                echo json_encode([
-                    "error" => "An error occured",
-                    "code" => 404,
-                    "message" => "The specified ID does not exist."
-                ]);
-            } finally {
-                exit;
-            }
-        }
-
-        if ($this->uriPartsCount === 3 && $this->uriParts[1] === "quizz" && $this->method === "GET") {
-            try {
-                $questionId = $this->uriParts[2];
-                http_response_code(200);
-                echo json_encode($this->crud->getOneQuestion($questionId));
-            } catch (InvalidArgumentException $e) {
-                http_response_code(400);
-                echo json_encode([
-                    "error" => "An error occured",
-                    "code" => 400,
-                    "message" => "The specified ID is not valid."
-                ]);
-            } catch (OutOfBoundsException $e) {
-                http_response_code(404);
-                echo json_encode([
-                    "error" => "An error occured",
-                    "code" => 404,
-                    "message" => "The specified ID does not exist."
-                ]);
-            } finally {
-                exit;
-            }
-        }
         if ($uri === "/quizz" && $method === "POST") {
             $data = json_decode(file_get_contents('php://input'), true);
             try {
@@ -98,7 +54,7 @@ class QuizzController
             }
         }
     }
-    
+
     private function checkCollectionVerbs()
     {
         if ($this->uri === "/quizz" && !in_array($this->method, self::ALLOWED_COLLECTION_VERBS)) {
@@ -118,6 +74,58 @@ class QuizzController
                 'error' => 'Verbs HTTP allowed are : ' . implode(", ", self::ALLOWED_RESOURCE_VERBS)
             ]);
             exit;
+        }
+    }
+
+    private function handleCollectionGet()
+    {
+        if ($this->uri === "/quizz" && $this->method === "GET") {
+            try {
+                http_response_code(200);
+                echo json_encode($this->crud->getAllQuestions());
+            } catch (InvalidArgumentException $e) {
+                http_response_code(400);
+                echo json_encode([
+                    "error" => "An error occured",
+                    "code" => 400,
+                    "message" => "The specified ID is not valid."
+                ]);
+            } catch (OutOfBoundsException $e) {
+                http_response_code(404);
+                echo json_encode([
+                    "error" => "An error occured",
+                    "code" => 404,
+                    "message" => "The specified ID does not exist."
+                ]);
+            } finally {
+                exit;
+            }
+        }
+    }
+    private function handleResourceGet()
+    {
+        if ($this->uriPartsCount === 3 && $this->uriParts[1] === "quizz" && $this->method === "GET") {
+            try {
+                $questionId = $this->uriParts[2];
+                http_response_code(200);
+                echo json_encode($this->crud->getOneQuestion($questionId));
+            } catch (InvalidArgumentException $e) {
+                http_response_code(400);
+                echo json_encode([
+                    "error" => "An error occured",
+                    "code" => 400,
+                    "message" => "The specified ID is not valid."
+                ]);
+            } catch (OutOfBoundsException) {
+                http_response_code(404);
+                echo json_encode([
+                    "error" => "An error occured",
+                    "code" => 404,
+                    "message" => "The specified ID does not exist."
+                ]);
+            } finally {
+                exit;
+            }
         }
     }
 }
