f872945df4896708b7828ae9a88245a2fddf6bd2
Nouster <djebali.mohamed88@gmail.com>
2023-04-12 12:46:37 +0200

    used QuizzController to get questions

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\config\DbConfig;
+use App\controller\QuizzController;
 use App\crud\QuizzCrud;
 use App\exception\ExceptionHandler;
 use Symfony\Component\Dotenv\Dotenv;
@@ -24,81 +25,84 @@ header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
 
 ExceptionHandler::handleException();
 
-
-$crud = new QuizzCrud($pdo);
-
-if ($uri === "/quizz" && $method === "GET") {
-    echo json_encode($crud->getAllQuestions());
-    exit;
+if (str_contains($uri, "/quizz")) {
+    $quizzController = new QuizzController($pdo, $uri, $method, $uriParts, $uriPartsCount);
 }
 
-if ($uriPartsCount === 3 && $uriParts[1] === "quizz" && $method === "GET") {
-    $id = intval($uriParts[2]);
-    try {
-        echo json_encode($crud->getOneQuestion($id));
-    } catch (InvalidArgumentException $e) {
-        http_response_code(400);
-        echo json_encode([
-            "error" => "An error occured",
-            "code" => 400,
-            "message" => "The specified ID is not valid."
-        ]);
-    } catch (OutOfBoundsException $e) {
-        http_response_code(404);
-        echo json_encode([
-            "error" => "An error occured",
-            "code" => 404,
-            "message" => "The specified ID does not exist."
-        ]);
-    } finally {
-        exit;
-    }
-}
+// $crud = new QuizzCrud($pdo);
 
-if ($uri === '/quizz' && $method === 'POST') {
-    try {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $questionId = $crud->createQuestion($data);
-        http_response_code(201);
-        echo json_encode([
-            "uri" => "/quizz/" . $questionId,
-        ]);
-    } catch (InvalidArgumentException $e) {
-        http_response_code(422);
-        echo json_encode([
-            "error" => $e->getMessage(),
-            "message" => "Missing required parameters."
-        ]);
-    } catch (RuntimeException $e) {
-        http_response_code(422);
-        echo json_encode([
-            "error" => $e->getMessage(),
-            "message" => "Required parameters cannot be empty."
-        ]);
-    } finally {
-        exit;
-    }
-}
+// if ($uri === "/quizz" && $method === "GET") {
+//     echo json_encode($crud->getAllQuestions());
+//     exit;
+// }
 
-if ($uriPartsCount === 3 && $uriParts[1] === "quizz" && $method === "DELETE") {
-    try {
-        $id = intval($uriParts[2]);
-        echo json_encode($crud->deleteQuestion($id));
-    } catch (InvalidArgumentException $e) {
-        http_response_code(400);
-        echo json_encode([
-            "error" => "An error occured",
-            "code" => 400,
-            "message" => "The specified ID is not valid."
-        ]);
-    } catch (OutOfBoundsException $e) {
-        http_response_code(404);
-        echo json_encode([
-            "error" => "An error occured",
-            "code" => 404,
-            "message" => "The specified ID does not exist."
-        ]);
-    } finally {
-        exit;
-    }
-}
+// if ($uriPartsCount === 3 && $uriParts[1] === "quizz" && $method === "GET") {
+//     $id = intval($uriParts[2]);
+//     try {
+//         echo json_encode($crud->getOneQuestion($id));
+//     } catch (InvalidArgumentException $e) {
+//         http_response_code(400);
+//         echo json_encode([
+//             "error" => "An error occured",
+//             "code" => 400,
+//             "message" => "The specified ID is not valid."
+//         ]);
+//     } catch (OutOfBoundsException $e) {
+//         http_response_code(404);
+//         echo json_encode([
+//             "error" => "An error occured",
+//             "code" => 404,
+//             "message" => "The specified ID does not exist."
+//         ]);
+//     } finally {
+//         exit;
+//     }
+// }
+
+// if ($uri === '/quizz' && $method === 'POST') {
+//     try {
+//         $data = json_decode(file_get_contents('php://input'), true);
+//         $questionId = $crud->createQuestion($data);
+//         http_response_code(201);
+//         echo json_encode([
+//             "uri" => "/quizz/" . $questionId,
+//         ]);
+//     } catch (InvalidArgumentException $e) {
+//         http_response_code(422);
+//         echo json_encode([
+//             "error" => $e->getMessage(),
+//             "message" => "Missing required parameters."
+//         ]);
+//     } catch (RuntimeException $e) {
+//         http_response_code(422);
+//         echo json_encode([
+//             "error" => $e->getMessage(),
+//             "message" => "Required parameters cannot be empty."
+//         ]);
+//     } finally {
+//         exit;
+//     }
+// }
+
+// if ($uriPartsCount === 3 && $uriParts[1] === "quizz" && $method === "DELETE") {
+//     try {
+//         $id = intval($uriParts[2]);
+//         echo json_encode($crud->deleteQuestion($id));
+//     } catch (InvalidArgumentException $e) {
+//         http_response_code(400);
+//         echo json_encode([
+//             "error" => "An error occured",
+//             "code" => 400,
+//             "message" => "The specified ID is not valid."
+//         ]);
+//     } catch (OutOfBoundsException $e) {
+//         http_response_code(404);
+//         echo json_encode([
+//             "error" => "An error occured",
+//             "code" => 404,
+//             "message" => "The specified ID does not exist."
+//         ]);
+//     } finally {
+//         exit;
+//     }
+// }
--- src/controller/QuizzController.php
+++ src/controller/QuizzController.php
@@ -5,28 +5,29 @@ namespace App\controller;
 use App\crud\QuizzCrud;
 use InvalidArgumentException;
 use PDO;
+use RuntimeException;
 
 class QuizzController
 {
     private PDO $pdo;
     private string $uri;
-    private string $httpMethod;
+    private string $method;
     private array $uriParts;
     private int $uriPartsCount;
     private QuizzCrud $crud;
     public const ALLOWED_COLLECTION_VERBS = ["GET", "POST"];
     public const ALLOWED_RESOURCE_VERBS = ["GET", "PUT", "DELETE"];
 
-    public function __construct(PDO $pdo, string $uri, string $httpMethod, array $uriParts, int $uriPartsCount)
+    public function __construct(PDO $pdo, string $uri, string $method, array $uriParts, int $uriPartsCount)
     {
         $this->pdo = $pdo;
         $this->uri = $uri;
-        $this->httpMethod = $httpMethod;
+        $this->method = $method;
         $this->uriParts = $uriParts;
         $this->uriPartsCount = $uriPartsCount;
         $this->crud = new QuizzCrud($pdo);
 
-        if ($uri === "/actor" && !in_array($this->httpMethod, self::ALLOWED_COLLECTION_VERBS)) {
+        if ($uri === "/quizz" && !in_array($this->method, self::ALLOWED_COLLECTION_VERBS)) {
             http_response_code(405);
             echo json_encode([
                 'error' => 'Verbs HTTP allowed are : ' . implode(", ", self::ALLOWED_COLLECTION_VERBS)
@@ -34,7 +35,7 @@ class QuizzController
             exit;
         }
 
-        if (str_contains($uri, "/actor/") && !in_array($this->httpMethod, self::ALLOWED_RESOURCE_VERBS)) {
+        if (str_contains($uri, "/quizz/") && !in_array($this->method, self::ALLOWED_RESOURCE_VERBS)) {
             http_response_code(405);
             echo json_encode([
                 'error' => 'Verbs HTTP allowed are : ' . implode(", ", self::ALLOWED_RESOURCE_VERBS)
@@ -42,24 +43,10 @@ class QuizzController
             exit;
         }
 
-        if($uri === "/quizz" && $httpMethod === "GET"){
+        if ($uri === "/quizz" && $method === "GET") {
             http_response_code(200);
             echo json_encode($this->crud->getAllQuestions());
             exit;
-        }
-
-
-        if($uri === "/quizz" && $httpMethod === "POST"){
-            try {
-            http_response_code(201);
-            echo json_encode($this->crud->getAllQuestions());
-            exit;
-            }catch(InvalidArgumentException $e){
-                http_response_code(422);
-                echo json_encode([
-                    "error" => $e->getMessage(),
-                    "message" => "Missing required parameters."]);
-            }
         }
     }
 }
